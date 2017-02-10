5480499ddf60d652ce36c4674b87790a7a843d1d
Dan <daniel.sohval@gmail.com>
2017-02-10 13:53:27 -0500

    debugged test

--- src/components/AnimalSelect.js
+++ src/components/AnimalSelect.js
@@ -3,38 +3,39 @@ import React, {Component} from 'react';
 export default class extends Component {
 
     constructor() {
-        super();
-        this.state = {
+          super();
+          this.state = {
             selectedAnimal : ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.viewAnimal = this.viewAnimal.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({selectedAnimal: event.target.value});
-    }
-
-    viewAnimal() {
-        this.props.submitAnimal(this.state.selectedAnimal);
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.viewAnimal}>
-                <label>
-                  Select an Animal: 
-                  <select value={this.state.selectedAnimal} onChange={this.handleChange}>
-                    {
-                        this.props.animals.map((animal, idx)=> {
-                            return <option value={animal} key={idx}>{animal}</option>
-                        })
-                    }
-                  </select>
-                </label>
-                <input type="submit" value="View" />
-            </form>
-        );
-    }
-
-}
+          };
+          this.handleChange = this.handleChange.bind(this);
+          this.viewAnimal = this.viewAnimal.bind(this);
+     }
+ 
+     handleChange(event) {
+         this.setState({selectedAnimal: event.target.value});
+     }
+ 
+     viewAnimal(event) {
+         this.props.submitAnimal(this.state.selectedAnimal);
+         event ? event.preventDefault() : null;
+      }
+  
+      render() {
+          return (
+              <form onSubmit={this.viewAnimal}>
+                  <label>
+                   Select an Animal: 
+                   <select value={this.state.selectedAnimal} onChange={this.handleChange}>
+                     {
+                         this.props.animals.map((animal, idx)=> {
+                             return <option value={animal} key={idx}>{animal}</option>
+                         })
+                      }
+                    </select>
+                  </label>
+                 <input type="submit" value="View" />
+              </form>
+          );
+      }
+ 
+ }
