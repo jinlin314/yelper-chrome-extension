abb7412e382be46dc7dec547c395712691652c21
Kate Humphrey <kate@fullstackacademy.com>
2017-04-07 16:13:28 -0400

    false positive erased

--- tests/react.spec.js
+++ tests/react.spec.js
@@ -90,6 +90,7 @@ describe('React components', () => {
             expect(animalSelect.find('select').length).to.be.equal(1);
             // loops through each option in the select
             // determines if the option's key is equivalent to the animal
+            expect(animalSelect.find('option')).to.have.length(animals.length)
             animalSelect.find('option').forEach((animalOption, i) => {
                 expect(animalOption.key()).to.be.equal(animals[i])
                 expect(animalOption.text().trim()).to.be.equal(animals[i])
